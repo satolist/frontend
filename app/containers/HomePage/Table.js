c007e1e0d4cff3654e7cf6755d1c445eff9b6731
drys262 <koeichavez17@gmail.com>
2020-08-15 19:38:27 +0800

    feat: setup table with pagination

---
+++ app/containers/HomePage/Table.js
@@ -0,0 +1,132 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import { useTable, usePagination } from 'react-table';
+import MaUTable from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+function Table({ columns, data }) {
+	const {
+		getTableProps,
+		getTableBodyProps,
+		headerGroups,
+		page,
+		prepareRow,
+
+		canPreviousPage,
+		canNextPage,
+		pageOptions,
+		pageCount,
+		gotoPage,
+		nextPage,
+		previousPage,
+		setPageSize,
+		state: { pageIndex, pageSize },
+	} = useTable(
+		{
+			columns,
+			data,
+			initialState: { pageIndex: 1 },
+		},
+		usePagination,
+	);
+
+	return (
+		<>
+			<MaUTable {...getTableProps()}>
+				<TableHead>
+					{headerGroups.map(headerGroup => (
+						<TableRow {...headerGroup.getHeaderGroupProps()}>
+							{headerGroup.headers.map(column => (
+								<TableCell {...column.getHeaderProps()}>
+									{column.render('Header')}
+								</TableCell>
+							))}
+						</TableRow>
+					))}
+				</TableHead>
+				<TableBody {...getTableBodyProps()}>
+					{page.map(row => {
+						prepareRow(row);
+						return (
+							<TableRow {...row.getRowProps()}>
+								{row.cells.map(cell => (
+									<TableCell {...cell.getCellProps()}>
+										{cell.render('Cell')}
+									</TableCell>
+								))}
+							</TableRow>
+						);
+					})}
+				</TableBody>
+			</MaUTable>
+
+			<div className="pagination">
+				<button
+					type="button"
+					onClick={() => gotoPage(0)}
+					disabled={!canPreviousPage}
+				>
+					{'<<'}
+				</button>{' '}
+				<button
+					type="button"
+					onClick={() => previousPage()}
+					disabled={!canPreviousPage}
+				>
+					{'<'}
+				</button>{' '}
+				<button
+					type="button"
+					onClick={() => nextPage()}
+					disabled={!canNextPage}
+				>
+					{'>'}
+				</button>{' '}
+				<button
+					type="button"
+					onClick={() => gotoPage(pageCount - 1)}
+					disabled={!canNextPage}
+				>
+					{'>>'}
+				</button>{' '}
+				<span>
+					Page{' '}
+					<strong>
+						{pageIndex + 1} of {pageOptions.length}
+					</strong>{' '}
+				</span>
+				<span>
+					| Go to page:{' '}
+					<input
+						type="number"
+						defaultValue={pageIndex + 1}
+						onChange={e => {
+							const selectedPage = e.target.value
+								? Number(e.target.value) - 1
+								: 0;
+							gotoPage(selectedPage);
+						}}
+						style={{ width: '100px' }}
+					/>
+				</span>{' '}
+				<select
+					value={pageSize}
+					onChange={e => {
+						setPageSize(Number(e.target.value));
+					}}
+				>
+					{[10, 20, 30, 40, 50].map(pageSizeOptions => (
+						<option key={pageSizeOptions} value={pageSizeOptions}>
+							Show {pageSizeOptions}
+						</option>
+					))}
+				</select>
+			</div>
+		</>
+	);
+}
+
+export default Table;
--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -6,15 +6,54 @@
  */
 
 import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { useQuery } from 'react-query';
 import Sidebar from './Sidebar';
+import Table from './Table';
+
+const fetchPhotos = async () => {
+	const response = await fetch('https://jsonplaceholder.typicode.com/photos');
+	return response.json();
+};
 
 export default function HomePage() {
-  return (
-    <div id="HomePage">
-      <Sidebar pageWrapId="page-wrap" outerContainerId="HomePage" />
-      <div id="page-wrap">
-        <div>Hello World</div>
-      </div>
-    </div>
-  );
+	const columns = React.useMemo(
+		() => [
+			{
+				Header: 'Album ID',
+				accessor: 'albumId', // accessor is the "key" in the data
+			},
+			{
+				Header: 'ID',
+				accessor: 'id',
+			},
+			{
+				Header: 'Thumbnail URL',
+				accessor: 'thumbnailUrl',
+			},
+			{
+				Header: 'Title',
+				accessor: 'title',
+			},
+			{
+				Header: 'URL',
+				accessor: 'url',
+			},
+		],
+		[],
+	);
+
+	const { data, status } = useQuery('photos', fetchPhotos);
+
+	return (
+		<div id="HomePage">
+			<Sidebar pageWrapId="page-wrap" outerContainerId="HomePage" />
+			<div id="page-wrap" className="container mx-auto">
+				<CssBaseline />
+				{status === 'error' && <div>Error fetching data</div>}
+				{status === 'loading' && <div>Loading ....</div>}
+				{status === 'success' && <Table columns={columns} data={data} />}
+			</div>
+		</div>
+	);
 }
